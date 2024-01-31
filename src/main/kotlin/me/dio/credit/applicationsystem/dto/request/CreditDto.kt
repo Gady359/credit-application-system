6450199f8d5908b3a78b433f1b22e3039d39103e
Gady359 <gady.peters@gmail.com>
2024-01-31 15:34:29 -0300

    Resolucao dos desafios de 48 parcelas e 3 meses, e problemas nas tabelas sql

--- src/main/kotlin/me/dio/credit/applicationsystem/dto/request/CreditDto.kt
+++ src/main/kotlin/me/dio/credit/applicationsystem/dto/request/CreditDto.kt
@@ -2,6 +2,10 @@ package me.dio.credit.applicationsystem.dto.request
 
 import java.math.BigDecimal
 import jakarta.persistence.*
+import jakarta.validation.Constraint
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+import jakarta.validation.Payload
 import jakarta.validation.constraints.Future
 import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.Min
@@ -10,11 +14,13 @@ import me.dio.credit.applicationsystem.entity.Credit
 import me.dio.credit.applicationsystem.entity.Customer
 import me.dio.credit.applicationsystem.enumeration.Status
 import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 import java.util.UUID
+import kotlin.reflect.KClass
 
 data class CreditDto(
     @field :NotNull(message ="Invalid input") val creditValue: BigDecimal,
-    @field :Future val dayFirstInstallment: LocalDate,
+    @field:MaxThreeMonths  val dayFirstInstallment: LocalDate,
     @field :Min(value = 1, message = "Number of installments cannot be zero")
     @field:Max(value = 48, message = "Number of installments cannot be greater than 48")
     val numberOfInstallments: Int,
@@ -27,4 +33,25 @@ data class CreditDto(
         customer = Customer(id = this.customerId)
 
     )
+}
+
+@Target(AnnotationTarget.FIELD, AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+@Constraint(validatedBy = [MaxThreeMonthsValidator::class])
+annotation class MaxThreeMonths(
+    val message: String = "The date must be in the future and within a maximum period of 3 months",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = []
+)
+
+class MaxThreeMonthsValidator : ConstraintValidator<MaxThreeMonths, LocalDate> {
+    override fun isValid(value: LocalDate?, context: ConstraintValidatorContext?): Boolean {
+        if (value == null) {
+            return true // Let @NotNull handle null check
+        }
+        val currentDate = LocalDate.now()
+        val maxDate = currentDate.plus(3, ChronoUnit.MONTHS)
+        return value.isAfter(currentDate) && value.isBefore(maxDate)
+    }
 }
